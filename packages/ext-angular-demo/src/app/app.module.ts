f08f4a6f8f981c8db991bd2fcb263c812b15baed
mgusmano <mgusmano@yahoo.com>
2018-11-10 12:45:38 -0500

    working demo

--- packages/ext-angular-demo/src/app/app.module.ts
+++ packages/ext-angular-demo/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ExtAngularModernModule } from './ext-angular-modern/ext-angular-modern'
+import { ExtAngularModernModule } from 'ext-angular-modern'
 
 @NgModule({
   declarations: [
