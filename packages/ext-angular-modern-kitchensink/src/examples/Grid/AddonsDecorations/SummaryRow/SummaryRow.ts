4c3b3a0669eb7b204b2865ef37331e2a6ec0e76d
aniket91 <aniketthakur91@gmail.com>
2018-11-24 15:05:30 +0530

    Adding Addon grid components

--- packages/ext-angular-modern-kitchensink/src/examples/Grid/AddonsDecorations/SummaryRow/SummaryRow.ts
+++ packages/ext-angular-modern-kitchensink/src/examples/Grid/AddonsDecorations/SummaryRow/SummaryRow.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {model} from '../../CompanyModel';
 
 declare var Ext: any;
+
+Ext.require([
+  'Ext.grid.plugin.SummaryRow',
+  'Ext.data.summary.Average',
+  'Ext.data.summary.Max',
+]);
 
 @Component({
   selector: 'summaryrow-component',
@@ -9,6 +16,30 @@ declare var Ext: any;
 })
 export class SummaryRowComponent implements OnInit {
 
+  store = Ext.create('Ext.data.Store', {
+    model,
+    autoLoad: true,
+    pageSize: 0,
+    proxy: {
+      type: 'ajax',
+      url: 'resources/data/CompanyData.json'
+    } 
+  });
+
+  renderSign = (format, value) => {
+    var formattedValue = Ext.util.Format.number(value, format);
+    var color = "";
+    if(value > 0) {
+      color = "green";
+    }
+    else {
+      color = "red";
+    }
+    return formattedValue;
+  } ;
+
+summarizeCompanies = (grid, context) => context.records.length + ' Companies';
+  
   constructor() { }
 
 
--- packages/ext-angular-modern-kitchensink/src/examples/Grid/AddonsDecorations/ViewOptions/ViewOptions.ts
+++ packages/ext-angular-modern-kitchensink/src/examples/Grid/AddonsDecorations/ViewOptions/ViewOptions.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {model} from '../../CompanyModel';
 
 declare var Ext: any;
+
+Ext.require(['Ext.grid.plugin.ViewOptions']);
 
 @Component({
   selector: 'videooprions-component',
@@ -9,9 +12,29 @@ declare var Ext: any;
 })
 export class ViewOptionsComponent implements OnInit {
 
+
+  store = Ext.create('Ext.data.Store', {
+    autoLoad: true,
+    pageSize: 0,
+    proxy: {
+      type: 'ajax',
+      url: 'resources/data/CompanyData.json'
+    } 
+  });
+
   constructor() { }
 
-
+  renderSign = (format, value) => {
+    var formattedValue = Ext.util.Format.number(value, format);
+    var color = "";
+    if(value > 0) {
+      color = "green";
+    }
+    else {
+      color = "red";
+    }
+    return formattedValue;
+  } ;
 
   ngOnInit() {
   }
