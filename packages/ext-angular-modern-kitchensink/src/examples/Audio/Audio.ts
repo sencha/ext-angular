a22118f73649b58f9cc6884d69c2dd395e25fbc7
aniket91 <aniketthakur91@gmail.com>
2018-11-26 17:53:04 +0530

    Adding remaining grid components

--- packages/ext-angular-modern-kitchensink/src/examples/Audio/Audio.ts
+++ packages/ext-angular-modern-kitchensink/src/examples/Audio/Audio.ts
@@ -12,29 +12,30 @@ export class AudioComponent implements OnInit {
   constructor() { }
 
   playing: boolean;
-  audio: AudioComponent;
+  audio: any;
   isAndroid = Ext.os.is.Android;
-
-  //@ViewChild('audioRef') audioRef: ElementRef<AudioComponent>;
 
   ngOnInit() {
     console.log("isAndroid : " + this.isAndroid);
-    //this is not correct. Need a proper way to reference audio.
-    //this.audio = this.audioRef.nativeElement;
   }
+
+
+  audioReady = (event) => {
+    console.log("audioReady");
+    this.audio = event.ext;
+  }
+
 
   toggleAudioAndroid() {
 
     if (this.playing) {
         console.log("Playing");
-        //this.audio.pause();
-        // audio.pause();
+        this.audio.pause();
     } else {
       console.log(" Not Playing");
-      //this.audio.play();
-      //audio.play();
+      this.audio.play();
     }        
-
+    
     this.playing = !this.playing;
     console.log("this.playing : " + this.playing);
   }
---
+++ packages/ext-angular-modern-kitchensink/src/examples/Grid/AdvancedFeatures/ReconfigureGrid/ReconfigureGrid.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var Ext: any;
+
+@Component({
+  selector: 'reconfiguregrid-component',
+  templateUrl: './ReconfigureGrid.html',
+  styles: [``]
+})
+export class ReconfigureGridComponent implements OnInit {
+
+  constructor() { }
+
+
+
+  ngOnInit() {
+  }
+
+
+}
