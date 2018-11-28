57ed9b0c2ca58eca3a9df5f05b38d8004981092f
alisha <alisha.agarwal@joshtechnologygroup.com>
2018-11-28 20:54:03 +0530

    Fixed Carousel component with setting height and width.

--- packages/ext-angular-modern-kitchensink/src/examples/Carousel/Carousel.ts
+++ packages/ext-angular-modern-kitchensink/src/examples/Carousel/Carousel.ts
@@ -15,17 +15,7 @@ export class CarouselComponent implements OnInit  {
   carouselInstance: any;
 
   compReady(event) {
-    console.log('############33', event.ext);
-    this.carouselInstance = event.ext;
-    this.carouselInstance.setData('items', this.carouselItems);
-    console.log('@@@@@@@@@@', this.carouselInstance);
   }
-  
-  carouselItems:any = [
-    {html: "<div>Swipe left to show the next card...</div>"},
-    {html: "<div #item [flex]=\"1\">You can also tap on either side of the indicators.</div>"},
-    {html: "<div #item [flex]=\"1\">Card #3</div>"} 
-  ]
 
   ngOnInit() {}
 
