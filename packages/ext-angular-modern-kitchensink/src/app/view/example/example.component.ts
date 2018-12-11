84b3aad1de2bae39cc72956682263302a9c0fa6c
aniket91 <aniketthakur91@gmail.com>
2018-12-11 13:50:35 +0530

    Adding hbox and vbox docs

--- packages/ext-angular-modern-kitchensink/src/app/view/example/example.component.ts
+++ packages/ext-angular-modern-kitchensink/src/app/view/example/example.component.ts
@@ -7,24 +7,16 @@
                 styles: [`
                         `],
                 template: `
-                        <panel #item shadow="true" [layout]="{type: 'vbox', align: 'stretch'}">
-                            <button #item [handler]="alertHandler" text="Alert"></button>
-                            <button #item [handler]="promptHandler" text="Prompt"></button>
-                            <button #item [handler]="confirmHandler" text="Confirm"></button>
-                        </panel>
+                        <container #item layout="hbox">
+                            <panel #item title="Inner Panel 1" flex="1">
+                                This is the inner panel content
+                            </panel>
+                            <panel #item title="Inner Panel 2" flex="1">
+                                This is the inner panel content
+                            </panel>
+                        </container>
                         `
             })
             export class AppComponent {
 
-                         onConfirmResult = function(buttonId, value, opt) {
-                             Ext.toast(`User clicked ${buttonId} button.`);
-                         }
-                
-                         onPromptResult = function(buttonId, value) {
-                             Ext.toast(`User clicked ${buttonId} and entered value "${value}".`);
-                         }
-
-                         alertHandler = () => Ext.Msg.alert('Title', 'The quick brown fox jumped over the lazy dog.');
-                         promptHandler = () => Ext.Msg.prompt('Welcome!', "What's your first name?", this.onPromptResult.bind(this));
-                         confirmHandler = () => Ext.Msg.confirm("Confirmation", "Are you sure you want to do that?", this.onConfirmResult.bind(this));
-            }
+             }
