7620319c806f23b57def0c94cf54dab5872f29fc
aniket91 <aniketthakur91@gmail.com>
2018-12-12 16:27:52 +0530

    Fixing few code window issues

--- packages/ext-angular-modern-kitchensink/src/app/view/landingpage/landingpage.component.ts
+++ packages/ext-angular-modern-kitchensink/src/app/view/landingpage/landingpage.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit, ChangeDetectorRef, ViewChild, ViewContainerRef,ElementRef, ComponentFactoryResolver, ComponentRef, ComponentFactory } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ViewChild, ViewEncapsulation, ViewContainerRef,ElementRef, ComponentFactoryResolver, ComponentRef, ComponentFactory } from '@angular/core';
 import {navTreeRoot} from '../../../examples';
 
 import { Location } from '@angular/common';
@@ -37,7 +37,108 @@ declare var _code: any;
 @Component({
   selector: 'app-root',
   templateUrl: 'landingpage.component.html',
-  styleUrls: ['./landingpage.component.css']
+  styles: [`
+
+  /*
+
+  Atom One Dark by Daniel Gamage
+  Original One Dark Syntax theme from https://github.com/atom/one-dark-syntax
+
+  base:    #282c34
+  mono-1:  #abb2bf
+  mono-2:  #818896
+  mono-3:  #5c6370
+  hue-1:   #56b6c2
+  hue-2:   #61aeee
+  hue-3:   #c678dd
+  hue-4:   #98c379
+  hue-5:   #e06c75
+  hue-5-2: #be5046
+  hue-6:   #d19a66
+  hue-6-2: #e6c07b
+
+  */
+
+  .hljs {
+    display: block;
+    overflow-x: auto;
+    padding: 0.5em;
+    color: #abb2bf;
+    background: #282c34;
+  }
+
+  .hljs-comment,
+  .hljs-quote {
+    color: #5c6370;
+    font-style: italic;
+  }
+
+  .hljs-doctag,
+  .hljs-keyword,
+  .hljs-formula {
+    color: #c678dd;
+  }
+
+  .hljs-section,
+  .hljs-name,
+  .hljs-selector-tag,
+  .hljs-deletion,
+  .hljs-subst {
+    color: #e06c75;
+  }
+
+  .hljs-literal {
+    color: #56b6c2;
+  }
+
+  .hljs-string,
+  .hljs-regexp,
+  .hljs-addition,
+  .hljs-attribute,
+  .hljs-meta-string {
+    color: #98c379;
+  }
+
+  .hljs-built_in,
+  .hljs-class .hljs-title {
+    color: #e6c07b;
+  }
+
+  .hljs-attr,
+  .hljs-variable,
+  .hljs-template-variable,
+  .hljs-type,
+  .hljs-selector-class,
+  .hljs-selector-attr,
+  .hljs-selector-pseudo,
+  .hljs-number {
+    color: #d19a66;
+  }
+
+  .hljs-symbol,
+  .hljs-bullet,
+  .hljs-link,
+  .hljs-meta,
+  .hljs-selector-id,
+  .hljs-title {
+    color: #61aeee;
+  }
+
+  .hljs-emphasis {
+    font-style: italic;
+  }
+
+  .hljs-strong {
+    font-weight: bold;
+  }
+
+  .hljs-link {
+    text-decoration: underline;
+  }
+
+  `],
+  // https://angular.io/api/core/ViewEncapsulation#None
+  encapsulation: ViewEncapsulation.None,
 })
 export class LandingpageComponent implements OnInit {
   showCode: boolean = false;
@@ -112,6 +213,7 @@ export class LandingpageComponent implements OnInit {
       next: (v) => {
         this.node = v;
         this.files = getFiles(v, _code);
+        console.log("files length : " + this.files.length);
         this.highlightCode();
         //console.log("Generating breadcrumb for ID: " + v.id);
         this.breadcrumb = generateBreadcrumb(v);
@@ -147,6 +249,7 @@ export class LandingpageComponent implements OnInit {
         //console.log("Children el length : " + this.node.childNodes.length);
 
         if (localNode) {
+          this.files = getFiles(localNode, _code);
           this.node$.next(localNode);
         } else {
           console.log("Not a valid node. Probably looking at resources");
