8bb73b876b8adb6e8034f91eff24b7b516cc92b8
aniket91 <aniketthakur91@gmail.com>
2018-12-12 19:35:44 +0530

    Fixing angular route

--- packages/ext-angular-tests/test/All_Tests/Simple.js
+++ packages/ext-angular-tests/test/All_Tests/Simple.js
@@ -1,6 +1,6 @@
 describe("Simple", function(){
     it('should render the button', function() {
-        //ST.navigate('#/simple')
+        ST.navigate('/simple')
         ST.component('#button')
             .visible()
             .text('Click Me')
