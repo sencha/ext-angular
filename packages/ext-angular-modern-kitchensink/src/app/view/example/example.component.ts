ebe87ae9355ce948e2caeef2c858329c5dd66e3f
aniket91 <aniketthakur91@gmail.com>
2018-12-13 11:13:22 +0530

    Adding 13 more angular docs

--- packages/ext-angular-modern-kitchensink/src/app/view/example/example.component.ts
+++ packages/ext-angular-modern-kitchensink/src/app/view/example/example.component.ts
@@ -7,16 +7,15 @@
                 styles: [`
                         `],
                 template: `
-                        <container #item layout="hbox">
-                            <panel #item title="Inner Panel 1" flex="1">
-                                This is the inner panel content
-                            </panel>
-                            <panel #item title="Inner Panel 2" flex="1">
-                                This is the inner panel content
-                            </panel>
-                        </container>
+                    <container #item layout="center">
+                        <formpanel #item shadow="true">
+                            <fieldset #item title="About You" instructions="Tell us about yourself." width="300">
+                                <textfield #item label="First Name" labelAlign="placeholder"></textfield>
+                                <textfield #item label="Last Name" labelAlign="placeholder"></textfield>
+                            </fieldset>
+                        </formpanel>
+                    </container>
                         `
             })
             export class AppComponent {
-
-             }
+            }
