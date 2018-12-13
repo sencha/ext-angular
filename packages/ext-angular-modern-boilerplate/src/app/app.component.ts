1fa630453c980b18ae359e902cb75649a24972b5
mgusmano <mgusmano@yahoo.com>
2018-12-13 07:26:12 -0500

    boilerplate mods

--- packages/ext-angular-modern-boilerplate/src/app/app.component.ts
+++ packages/ext-angular-modern-boilerplate/src/app/app.component.ts
@@ -3,12 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-<panel #item [title]=title [layout]="'hbox'" [fitToParent]="true">
-  <button #item [text]="'click'" ></button>
-</container>
+<panel #item [title]=title [layout]="'fit'" [fitToParent]="true">
+  <grid #item
+    [columns]="gridcolumns" 
+    [store]="gridstore"
+    (select)="onGridSelect($event)"
+  ></grid>
+</panel>
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'ext-angular-modern-boilerplate';
+  title = 'ext-angular-modern-boilerplate'
+  gridcolumns = [
+		{ text: 'Name', width: 250, dataIndex: 'name' },
+		{ text: 'Email Address', flex: 1, dataIndex: 'email' },
+		{ text: 'Phone Number', width: 250, dataIndex: 'phone' }
+	]
+	gridstore = {
+		fields: ['name', 'email', 'phone'],
+		data: [
+			{ name: 'Lisa', email: 'lisa@simpsons.com', phone: '555-111-1224' },
+			{ name: 'Bart', email: 'bart@simpsons.com', phone: '555-222-1234' },
+			{ name: 'Homer', email: 'homer@simpsons.com', phone: '555-222-1244' },
+			{ name: 'Marge', email: 'marge@simpsons.com', phone: '555-222-1254' }
+		]
+  }
+  private onGridSelect({record}) {
+		alert(record.data.name)
+	}
 }
