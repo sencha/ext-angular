fe2a1b82d48c7a5cfee13ed3411ce0ae4dff1e1c
alisha <alisha.agarwal@joshtechnologygroup.com>
2018-12-24 14:06:21 +0530

    Updated to extract all tags from html files.

--- packages/ext-angular-webpack-plugin/src/angularUtil.js
+++ packages/ext-angular-webpack-plugin/src/angularUtil.js
@@ -61,17 +61,97 @@ export function extractFromSource(module, options, compilation) {
     const logv = require('./pluginUtil').logv
     logv(options,'FUNCTION extractFromSource')
     var statements = []
-    var prefix = '<ext-'
-    for (var i = 0; i < js.length; ++i) {
-      if (js.substring(i, i + prefix.length) == prefix) {
-        var start = js.substring(i)
-        var end = start.indexOf(' ')
-        var xtype = start.substring(prefix.length,end)
-        var type = { xtype: toXtype(xtype) }
-        let config = JSON.stringify(type)
-        statements.push(`Ext.create(${config})`)
+    if (module.resource.match(/\.html$/)) {
+
+      var generate = require("@babel/generator").default
+      var parse = require("babylon").parse
+      var traverse = require("ast-traverse")
+
+      var ast = parse(js, {
+        plugins: [
+          'typescript',
+          'flow',
+          'doExpressions',
+          'objectRestSpread',
+          'classProperties',
+          'exportDefaultFrom',
+          'exportExtensions',
+          'asyncGenerators',
+          'functionBind',
+          'functionSent',
+          'dynamicImport'
+        ],
+        sourceType: 'module'
+      })
+
+      function addType(argNode) {
+        var type
+
+        if (argNode.type === 'StringLiteral') {
+          var xtype = toXtype(argNode.value)
+
+          if (xtype != 'extreact') {
+            type = {
+              xtype: toXtype(argNode.value)
+            }
+          }
+        } else {
+          type = {
+            xclass: js.slice(argNode.start, argNode.end)
+          }
+        }
+
+        if (type != undefined) {
+          let config = JSON.stringify(type)
+          statements.push(`Ext.create(${config})`)
+        }
       }
+
+      traverse(ast, {
+        pre: function (node) {
+          if (node.type === 'CallExpression' && node.callee && node.callee.object && node.callee.object.name === 'Ext') {
+            statements.push(generate(node).code)
+          }
+          if(node.type === 'StringLiteral') {
+            let code = node.value
+            for (var i = 0; i < code.length; ++i) {
+              if (code.charAt(i) == '<') {
+                if (resource.match(/\.html$/) && code.substr(i, 4) == '<!==') {
+                  i += 4
+                  i += code.substr(i).indexOf('-->') + 3
+                  continue
+                } else if (code.charAt(i+1) !== '/') {
+                  var start = code.substring(i)
+                  var end = start.indexOf(' ')
+                  if (end >= 0) {
+                    var xtype = start.substring(1, end)
+                    // TODO add condition to check for ext componenets only.
+                    // Donot need toXtype here since it can safely be assumend the selector is same as xtype for all components defined.
+                    var type = { xtype: toXtype(xtype) }
+                    let config = JSON.stringify(type)
+                    statements.push(`Ext.create(${config})`)
+                  }
+                }
+              }
+              // Probably not required
+              if (node.type == 'VariableDeclarator' && node.init && node.init.type == 'CallExpression' && node.init.callee) {
+                if (node.init.callee.name == 'reactify') {
+                  for (let i = 0; i < node.init.arguments.length; i++) {
+                    const valueNode = node.init.arguments[i]
+                    if (!valueNode) continue
+                    addType(valueNode)
+                  }
+                }
+              }
+            }
+          }
+        }
+      })
+
     }
+
+    console.log('$$$$$$$$$$$$$$$4 statements are')
+    console.log(statements)
     return statements
   }
   catch(e) {
