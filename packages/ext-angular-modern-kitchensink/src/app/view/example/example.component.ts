68498eab61b031d3e3e735dd23321c9cba04feee
aniket91 <aniketthakur91@gmail.com>
2019-01-10 16:46:18 +0530

    Fixing editable and cellediting issue of editor

--- packages/ext-angular-modern-kitchensink/src/app/view/example/example.component.ts
+++ packages/ext-angular-modern-kitchensink/src/app/view/example/example.component.ts
@@ -1,32 +1,65 @@
-            // @example packages=[angular]
             import { Component } from '@angular/core'
             declare var Ext: any;
 
+            Ext.require('Ext.grid.plugin.Editable');
             @Component({
                 selector: 'app-root-1',
                 styles: [`
                         `],
                 template: `
-                    <formpanel #item shadow="true" layout="vbox" maxWidth="850">
-                        <sliderfield #item label="Value" width="350" (change)="updateGauges($event)" [value]="needleValue">
-                        </sliderfield>
-                        <container #item
-                            [layout]="{type: 'hbox', align: 'stretch'}"
-                            [margin]="'10 0 10 0'"
+                <container #item>
+                    <grid #item
+                        [height]="'275px'"
+                        [store]="this.store"
+                        [plugins]="['grideditable']"
+                    >
+                        <column #item 
+                            text="First Name"
+                            dataIndex="fname"
                             flex="1"
-                            [width]="'100%'"
-                            minHeight="200"
+                            editable="true"
+                        ></column>
+                        <column #item
+                            text="Last Name"
+                            dataIndex="lname"
+                            flex="1"
+                            editable="true"
+                        ></column>
+                        <column #item
+                            text="Talent"
+                            dataIndex="talent"
+                            flex="1"
+                            editable="true"
+                            (ready)="readyColumn($event)"
                         >
-                             <gauge #item flex="1" [value]="needleValue" 
-                                [needle]="{outerRadius: '100%'}"
-                                [valueStyle]="{display: 'none'}"
-                            ></gauge>
-                        </container>
-                    </formpanel>
+                        </column>
+                    </grid>
+                </container>
                 `
             })
             export class AppComponent {
-                onChange = (param) => {
-                    Ext.Msg.alert('Color', param.color);
-                }
+                store = new Ext.data.Store({
+                    data: [
+                        { 'fname': 'Barry',  'lname': 'Allen', 'talent': 'Speedster'},
+                        { 'fname': 'Oliver', 'lname': 'Queen', 'talent': 'Archery'},
+                        { 'fname': 'Kara',   'lname': 'Zor-El', 'talent': 'All'},
+                        { 'fname': 'Helena', 'lname': 'Bertinelli', 'talent': 'Weapons Expert'},
+                        { 'fname': 'Hal',    'lname': 'Jordan', 'talent': 'Willpower'  }
+                    ]
+                });
+                talentColumn:any;
+                readyColumn = (event) => {
+                    this.talentColumn = event.ext;
+                    this.talentColumn.setEditor({
+                        xtype: 'selectfield',
+                        options: [
+                            { text: 'All', value: 'All' },
+                            { text: 'Archery', value: 'Archery' },
+                            { text: 'Speedster', value: 'Speedster' },
+                            { text: 'Weapons', value: 'Weapons' },
+                            { text: 'Willpower', value: 'Willpower' }
+                        ]
+                    });
+                  }
             }
+
