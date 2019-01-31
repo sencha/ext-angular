b240a8ed7f3f5cbdbfed88964cfb452fa68239ff
David Alley <dalley@kickdrumtech.com>
2019-01-31 11:21:34 -0600

    EXTANG-17: Updates for Pie Charts

--- packages/ext-angular-kitchensink/src/examples/Charts/Pie/DoubleDonut/DoubleDonut.ts
+++ packages/ext-angular-kitchensink/src/examples/Charts/Pie/DoubleDonut/DoubleDonut.ts
@@ -1,62 +1,60 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import {innerData, outerData} from './createData';
-
 declare var Ext: any;
+import { Component } from '@angular/core';
+import { innerData, outerData } from './createData';
 
 @Component({
   selector: 'DoubleDonut-component',
   templateUrl: './DoubleDonut.html',
   styles: [``]
 })
-export class DoubleDonutComponent implements OnInit {
+export class DoubleDonutComponent {
 
-  constructor() { }
+  constructor() {
+  }
 
   isPhone = Ext.os.is.Phone;
 
-
-
-  ngOnInit() {
-  }
-
   outerStore = Ext.create('Ext.data.Store', {
     data: outerData
-})
-innerStore = Ext.create('Ext.data.Store', {
+  });
+
+  innerStore = Ext.create('Ext.data.Store', {
     data: innerData
-})
+  });
 
-theme:string= 'default';
+  theme: string = 'default';
 
-changeTheme = theme => this.theme = theme;
+  changeTheme = theme => this.theme = theme;
 
-polarSeries = [{
+  polarSeries = [{
     type: 'pie',
     angleField: 'usage',
     label: {
-        field: 'type',
-        display: 'inside'
+      field: 'type',
+      display: 'inside'
     },
     store: this.innerStore,
     radiusFactor: 70,
     donut: 20,
     tooltip: {
-        trackMouse: true,
-        renderer: (tooltip, record) => { tooltip.setHtml(Ext.String.capitalize(record.get('type')) + ' sector: ' + record.get('usage')); }
+      trackMouse: true,
+      renderer: (tooltip, record) => {
+        tooltip.setHtml(Ext.String.capitalize(record.get('type')) + ' sector: ' + record.get('usage'));
+      }
     }
-}, {
+  }, {
     type: 'pie',
     angleField: 'usage',
     donut: 80,
     highlight: true,
-    label: { field: 'provider' },
+    label: {field: 'provider'},
     tooltip: {
-        trackMouse: true,
-        renderer: (tooltip, record) => { tooltip.setHtml(record.get('provider') + ': ' + record.get('usage')); }
+      trackMouse: true,
+      renderer: (tooltip, record) => {
+        tooltip.setHtml(record.get('provider') + ': ' + record.get('usage'));
+      }
     }
-}];
-
-
+  }];
 
 
 }
