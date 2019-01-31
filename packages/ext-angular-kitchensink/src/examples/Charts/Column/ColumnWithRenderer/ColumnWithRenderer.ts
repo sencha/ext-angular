cd083b1010a44d1fe9cc614cb138f23d7e1fda63
David Alley <dalley@kickdrumtech.com>
2019-01-31 11:49:04 -0600

    EXTANG-17: Updates for Column Charts

--- packages/ext-angular-kitchensink/src/examples/Charts/Column/ColumnWithRenderer/ColumnWithRenderer.ts
+++ packages/ext-angular-kitchensink/src/examples/Charts/Column/ColumnWithRenderer/ColumnWithRenderer.ts
@@ -1,73 +1,66 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import {seriesG1Renderer, seriesG2Renderer} from './renderer';
-import {createData} from './createData';
-
-
 declare var Ext: any;
+import { Component } from '@angular/core';
+import { seriesG1Renderer, seriesG2Renderer } from './renderer';
+import { createData } from './createData';
 
 @Component({
   selector: 'ColumnWithRenderer-component',
   templateUrl: './ColumnWithRenderer.html',
   styles: [``]
 })
-export class ColumnWithRendererComponent implements OnInit {
+export class ColumnWithRendererComponent {
 
   constructor() {
-      this.refreshData();
-   }
-
-
-
-  ngOnInit() {
+    this.refreshData();
   }
 
-   isPhone = Ext.os.is.Phone;
+  isPhone = Ext.os.is.Phone;
 
-   store = Ext.create('Ext.data.Store', {
+  store = Ext.create('Ext.data.Store', {
     fields: ['id', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'name']
-})
+  });
 
-numRecords:number= 10;
+  numRecords: number = 10;
 
-refreshData = () => {
+  refreshData = () => {
     this.store.loadData(createData(this.numRecords));
-}
+  };
 
-cartesianAxes = [{
+  cartesianAxes = [{
     type: 'numeric',
     position: 'left',
     fields: ['g1', 'g2'],
     minimum: 0
-}, {
+  }, {
     type: 'category',
     position: 'bottom',
     fields: 'name'
-}];
+  }];
 
-cartesianSeries = [{
+  cartesianSeries = [{
     type: 'bar',
     xField: 'name',
     yField: 'g1',
     renderer: seriesG1Renderer,
     style: {
-        lineWidth: 2,
-        maxBarWidth: 30,
-        stroke: 'dodgerblue',
-        fill: 'palegreen',
-        opacity: 0.6
+      lineWidth: 2,
+      maxBarWidth: 30,
+      stroke: 'dodgerblue',
+      fill: 'palegreen',
+      opacity: 0.6
     }
-}, {
+  }, {
     type: 'bar',
     xField: 'name',
     yField: ['g2'],
     renderer: seriesG2Renderer,
     style: {
-        lineWidth: 2,
-        maxBarWidth: 12,
-        stroke: 'tomato',
-        fill: 'lightyellow',
-        radius: 20
+      lineWidth: 2,
+      maxBarWidth: 12,
+      stroke: 'tomato',
+      fill: 'lightyellow',
+      radius: 20
     }
-}];
+  }];
 
 }
--- packages/ext-angular-kitchensink/src/examples/Charts/Column/MultiaxisColumn/MultiaxisColumn.ts
+++ packages/ext-angular-kitchensink/src/examples/Charts/Column/MultiaxisColumn/MultiaxisColumn.ts
@@ -1,157 +1,136 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import {createData}  from './createData';
-
 declare var Ext: any;
+import { Component } from '@angular/core';
+import { createData } from './createData';
 
 @Component({
   selector: 'MultiaxisColumn-component',
   templateUrl: './MultiaxisColumn.html',
   styles: [``]
 })
-export class MultiaxisColumnComponent implements OnInit {
+export class MultiaxisColumnComponent {
 
   constructor() {
-
     this.refreshData();
-   }
-
-
-
-  ngOnInit() {
   }
 
-   isPhone = Ext.os.is.Phone;
+  isPhone = Ext.os.is.Phone;
 
-   store = Ext.create('Ext.data.Store', {
+  store = Ext.create('Ext.data.Store', {
     fields: [
-        'month',
-        'high',
-        'low',
-        {
-            name: 'highF',
-            calculate: function (data) {
-                return data.high * 1.8 + 32;
-            }
-        },
-        {
-            name: 'lowF',
-            calculate: function (data) {
-                return data.low * 1.8 + 32;
-            }
+      'month',
+      'high',
+      'low',
+      {
+        name: 'highF',
+        calculate: function (data) {
+          return data.high * 1.8 + 32;
         }
+      },
+      {
+        name: 'lowF',
+        calculate: function (data) {
+          return data.low * 1.8 + 32;
+        }
+      }
     ]
-})
+  });
 
-refreshData = () => {
+  refreshData = () => {
     this.store.loadData(createData());
-}
+  };
 
-cartesianAxes = [{
-    type: 'numeric',
-    //id: 'fahrenheit-axis2',
-    adjustByMajorUnit: true,
-    position: 'left',
-    titleMargin: 20,
-    minimum: 32,
-    grid: true,
-    title: 'Temperature in °F',
-    // listeners: {
-    //     rangechange: (axis, range) => {
-    //         //const cAxis = axis.getChart().getAxis('celsius-axis');
-    //         const cAxis = axis.getChart().getAxis(0);
-    //         if(cAxis) {
-    //             cAxis.setMinimum((range[0] - 32) / 1.8);
-    //             cAxis.setMaximum((range[1] - 32) / 1.8);
-    //         }
-    //     }
-    // }
-},
-{
-    type: 'numeric',
-    //id: 'celsius-axis',
-    titleMargin: 20,
-    position: 'right',
-    title: 'Temperature in °C'
-},
-{
-    type: 'category',
-    id: 'months-axis',
-    position: 'bottom'
-},
-{
-    position: 'top',
-    linkedTo: 'months-axis',
-    title: {
+  cartesianAxes = [
+    {
+      type: 'numeric',
+      adjustByMajorUnit: true,
+      position: 'left',
+      titleMargin: 20,
+      minimum: 32,
+      grid: true,
+      title: 'Temperature in °F',
+    },
+    {
+      type: 'numeric',
+      titleMargin: 20,
+      position: 'right',
+      title: 'Temperature in °C'
+    },
+    {
+      type: 'category',
+      id: 'months-axis',
+      position: 'bottom'
+    },
+    {
+      position: 'top',
+      linkedTo: 'months-axis',
+      title: {
         text: 'Climate data for Redwood City, California',
         fontSize: 18,
         fillStyle: 'green'
+      }
     }
-}
-];
+  ];
 
 
-cartesianGradients = [{
+  cartesianGradients = [{
     id: 'rainbow',
     type: 'linear',
     degrees: 270,
     stops: [
-        {
-            offset: 0,
-            color: '#78C5D6'
-        },
-        {
-            offset: 0.14,
-            color: '#449AA7'
-        },
-        {
-            offset: 0.28,
-            color: '#79C267'
-        },
-        {
-            offset: 0.42,
-            color: '#C4D546'
-        },
-        {
-            offset: 0.56,
-            color: '#F5D63D'
-        },
-        {
-            offset: 0.70,
-            color: '#F18B32'
-        },
-        {
-            offset: 0.84,
-            color: '#E767A1'
-        },
-        {
-            offset: 1,
-            color: '#BF62A6'
-        }
+      {
+        offset: 0,
+        color: '#78C5D6'
+      },
+      {
+        offset: 0.14,
+        color: '#449AA7'
+      },
+      {
+        offset: 0.28,
+        color: '#79C267'
+      },
+      {
+        offset: 0.42,
+        color: '#C4D546'
+      },
+      {
+        offset: 0.56,
+        color: '#F5D63D'
+      },
+      {
+        offset: 0.70,
+        color: '#F18B32'
+      },
+      {
+        offset: 0.84,
+        color: '#E767A1'
+      },
+      {
+        offset: 1,
+        color: '#BF62A6'
+      }
     ]
-}];
+  }];
 
-cartesianSeries = [{
+  cartesianSeries = [{
     type: 'bar',
     xField: 'month',
     yField: 'highF',
-    //yAxis: 'fahrenheit-axis',
     yAxis: 0,
     style: {
-        minGapWidth: 10,
-        strokeStyle: 'rgb(52, 52, 53)'
+      minGapWidth: 10,
+      strokeStyle: 'rgb(52, 52, 53)'
     },
-    subStyle: { fillStyle: 'url(#rainbow)' }
-}, {
+    subStyle: {fillStyle: 'url(#rainbow)'}
+  }, {
     type: 'bar',
     xField: 'month',
     yField: ['lowF'],
-    //yAxis: 'fahrenheit-axis',
     yAxis: 0,
     style: {
-        minGapWidth: 10,
-        strokeStyle: 'rgb(52, 52, 53)'
+      minGapWidth: 10,
+      strokeStyle: 'rgb(52, 52, 53)'
     }
-}];
-
+  }];
 
 }
