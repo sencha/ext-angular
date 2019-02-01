cdda0a2dd6c5f3b0c714dd1ddc4398dc07d620c6
mgusmano <mgusmano@yahoo.com>
2019-02-01 11:57:19 -0500

    typescript

--- packages/ext-angular-kitchensink/src/main.ts
+++ packages/ext-angular-kitchensink/src/main.ts
@@ -1,7 +1,5 @@
 import { enableProdMode } from '@angular/core'
 import { bootstrapModule } from '@sencha/ext-angular/esm5/lib/ext-angular-bootstrap.component';
 import {AppModule} from './app/app.module';
-import { environment } from './environments/environment'
 
-if (environment.production) {enableProdMode()}
-bootstrapModule(AppModule);
+bootstrapModule( AppModule );
