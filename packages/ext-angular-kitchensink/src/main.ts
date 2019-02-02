30f9f8ddbe539f62892743b880f10673664a3e78
mgusmano <mgusmano@yahoo.com>
2019-02-02 08:21:22 -0500

    treeshake mods

--- packages/ext-angular-kitchensink/src/main.ts
+++ packages/ext-angular-kitchensink/src/main.ts
@@ -1,5 +1,5 @@
 import { enableProdMode } from '@angular/core'
 import { bootstrapModule } from '@sencha/ext-angular/esm5/lib/ext-angular-bootstrap.component';
-import {AppModule} from './app/app.module';
+import { AppModule } from './app/app.module';
 
 bootstrapModule( AppModule );
