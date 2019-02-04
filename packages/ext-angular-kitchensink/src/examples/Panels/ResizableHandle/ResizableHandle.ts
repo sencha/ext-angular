b95db0281a5506654e249879410af6feac8c6792
mgusmano <mgusmano@yahoo.com>
2019-02-04 10:21:27 -0500

    resizable handle

--- packages/ext-angular-kitchensink/src/examples/Panels/ResizableHandle/ResizableHandle.ts
+++ packages/ext-angular-kitchensink/src/examples/Panels/ResizableHandle/ResizableHandle.ts
@@ -4,12 +4,17 @@ import { mediumText } from '../../dummy';
 //https://www.sencha.com/forum/showthread.php?468441-6-5-Modern-Resizable-panel-throw-an-exception-when-dragging-ne-se-sw-nw-handles
 
 @Component({
-  selector: 'resizable-panel-component',
+  selector: 'resizable-handle-component',
   templateUrl: "./ResizableHandle.html",
   styles: [``]
 })
 
 export class ResizableHandleComponent {
+
+  bodyText:any;
+  constructor() {
+    this.bodyText = mediumText;
+  }
 
   doDynamically:boolean = false;
   toggleDynamic = (event) => {
