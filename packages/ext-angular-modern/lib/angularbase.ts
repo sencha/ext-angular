b9f6c3704261831edd490b9048dfcb4dda08c2b8
mgusmano <mgusmano@yahoo.com>
2019-12-19 16:43:25 +0000

    angular

--- packages/ext-angular-modern/lib/angularbase.ts
+++ packages/ext-angular-modern/lib/angularbase.ts
@@ -142,8 +142,9 @@ export class EngBase {
             // this.node.newDiv.A.ext.add(w)
           }
           else if (this.node.innerHTML.substring(0, 4) != '<ext' &&
-                   this.node.innerHTML.substring(0, 4) != '<!--'
-          ) {
+            this.node.innerHTML.substring(0, 4) != '<!--' &&
+            this.node.innerHTML.substring(0, 4) != '<rou')
+            {
             var el = Ext.get(this.node.childNodes.item(0));
             var w = Ext.create({xtype:'widget', element: el});
             this.node.newDiv.A.ext.add(w)
