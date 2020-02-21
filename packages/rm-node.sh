c4655597a1f1281f151ba39277e2b1100bbeda6b
mgusmano <mgusmano@yahoo.com>
2020-02-21 14:18:16 -0500

    bundles

--- packages/rm-node.sh
+++ packages/rm-node.sh
@@ -1,6 +1,8 @@
-rm -r ext-angular/node_modules
+
 rm -r ext-angular-boilerplate/node_modules
+rm -r ext-angular-classic/node_modules
 rm -r ext-angular-demo/node_modules
 rm -r ext-angular-gen/node_modules
 rm -r ext-angular-kitchensink/node_modules
+rm -r ext-angular-modern/node_modules
 rm -r ext-angular-tests/node_modules
